f9be09d9dd17da18c663daa63665285a645bcb9f
NamBlue <namblue@me.com>
2017-01-19 15:35:10 -0800

    Initial Commit

---
+++ app/src/main/java/test/a00965170/comp3717/bcit/ca/tutorial/MainActivity.java
@@ -0,0 +1,21 @@
+package test.a00965170.comp3717.bcit.ca.tutorial;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Get fullscreen and no title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(new GamePanel(this));
+    }
+}
